f0ba852318b4abd88d890d33983502153dc27f45
Shiru99 <shriram.ghadge1999@gmail.com>
2021-06-26 12:46:34 +0530

    Bean scope

---
+++ SpringBasics/src/io/summer/Part7/DrawingShapes.java
@@ -0,0 +1,26 @@
+package io.summer.Part7;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingShapes {
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("io/summer/Part7/spring.xml");
+
+        Triangle scalarTriangle = (Triangle) applicationContext.getBean("ScalarTriangle");
+        scalarTriangle.draw();
+
+        Triangle scalarTriangle0 = (Triangle) applicationContext.getBean("ScalarTriangle0");
+        scalarTriangle0.draw();
+
+        System.out.println("Updating co-ordinates :");
+        scalarTriangle.p2.setX(-99);
+        scalarTriangle.p2.setY(-99);
+        scalarTriangle.draw();
+        scalarTriangle0.draw();
+        
+        ((AbstractApplicationContext) applicationContext).close();
+    }
+}
