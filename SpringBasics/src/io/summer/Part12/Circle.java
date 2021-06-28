8d58a7ab467a93cfccc94bce260039b9f35ae5c5
Shiru99 <shriram.ghadge1999@gmail.com>
2021-06-28 08:22:50 +0530

    using Interfaces

---
+++ SpringBasics/src/io/summer/Part12/Circle.java
@@ -0,0 +1,23 @@
+package io.summer.Part12;
+
+public class Circle extends Shape {
+
+    String shapeType = "Circle";
+    Point center;
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
+    @Override
+    public void draw() {
+        System.out.print("Drawing Circle with center - ");
+        getCenter().pointDetails();
+        System.out.println();
+    }
+    
+}
---
+++ SpringBasics/src/io/summer/Part12/Shape.java
@@ -0,0 +1,8 @@
+package io.summer.Part12;
+
+abstract class Shape {
+    
+    String shapeType;
+
+    abstract public void draw();
+}
