9e053b150ee208d4fb0d914d86097e935efdad44
Shiru99 <shriram.ghadge1999@gmail.com>
2021-06-28 21:53:43 +0530

    JSR 250 : @Resource

--- SpringBasics/src/io/summer/Part14/Circle.java
+++ SpringBasics/src/io/summer/Part14/Circle.java
@@ -1,14 +1,19 @@
 package io.summer.Part14;
 
+import jakarta.annotation.Resource;
+
 public class Circle extends Shape {
 
     String shapeType = "Circle";
+
     Point center;
 
     public Point getCenter() {
         return center;
     }
 
+    // @Resource : By Default checks for beanName - center
+    @Resource
     public void setCenter(Point center) {
         this.center = center;
     }
--- SpringBasics/src/io/summer/Part14/DrawingShapes.java
+++ SpringBasics/src/io/summer/Part14/DrawingShapes.java
@@ -12,6 +12,5 @@ public class DrawingShapes {
         shape.draw();
 
         applicationContext.close();
-        
     }
 }
