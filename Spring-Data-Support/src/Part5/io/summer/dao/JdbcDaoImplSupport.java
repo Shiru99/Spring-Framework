023d0b5ad582f80a11926c99f56a7cb3413972ab
Shiru99 <shriram.ghadge1999@gmail.com>
2021-07-06 15:24:19 +0530

    DAO Support Class for JDBC Template

--- Spring-Data-Support/src/Part5/io/summer/dao/JdbcDaoImplSupport.java
+++ Spring-Data-Support/src/Part5/io/summer/dao/JdbcDaoImplSupport.java
@@ -4,37 +4,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Component;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import Part5.io.summer.model.Circle;
 
-@Component
-public class JdbcDaoImplSupport {
-
-    private JdbcTemplate jdbcTemplate;
-
-    public JdbcTemplate getJdbcTemplate() {
-        return jdbcTemplate;
-    }
-
-    private DataSource dataSource;
-
-    public DataSource getDataSource() {
-        return dataSource;
-    }
-
-    @Autowired
-    public void setDataSource(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
+public class JdbcDaoImplSupport extends JdbcDaoSupport {
 
     public List<Circle> getAllCircles() {
-        return jdbcTemplate.query("SELECT * FROM circle", new CircleMapper());
+        return this.getJdbcTemplate().query("SELECT * FROM circle", new CircleMapper());
     }
 
     private static final class CircleMapper implements RowMapper<Circle> {
